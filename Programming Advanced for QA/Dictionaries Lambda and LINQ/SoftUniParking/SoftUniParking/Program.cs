28d486cec28a02e35c4b609b5e214b3d12c51b3a
TeodorKrustev <teodorrkrustev@abv.bg>
2024-05-08 10:50:35 +0300

    Add files

---
+++ Programming Advanced for QA/Dictionaries Lambda and LINQ/SoftUniParking/SoftUniParking/Program.cs
@@ -0,0 +1,43 @@
+﻿Dictionary<string, string> registrars = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] commandArray = Console.ReadLine().Split().ToArray();
+
+    string command = commandArray[0];
+    string employee = commandArray[1];
+
+    if (command == "register")
+    {
+        string licensePlateNumber = commandArray[2];
+
+        if (!registrars.ContainsKey(employee))
+        {
+            registrars.Add(employee, licensePlateNumber);
+            Console.WriteLine($"{employee} registered {licensePlateNumber} successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+        }
+    }
+    else if (command == "unregister")
+    {
+        if (registrars.ContainsKey(employee))
+        {
+            registrars.Remove(employee);
+            Console.WriteLine($"{employee} unregistered successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: user {employee} not found");
+        }
+    }
+}
+
+foreach (KeyValuePair<string, string> pair in registrars)
+{
+    Console.WriteLine($"{pair.Key} => {pair.Value}");
+}
---
+++ Programming Advanced for QA/Objects and Classes/Students/Students/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Students
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = homeTown;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Student> students = new List<Student>();
+
+            while (command != "end")
+            {
+                string[] studentData = command.Split(" ");
+
+                if (studentData.Length == 4)
+                {
+                    string firstName = studentData[0];
+                    string lastName = studentData[1];
+
+                    if (int.TryParse(studentData[2], out int age))
+                    {
+                        string homeTown = studentData[3];
+
+                        Student currentStudent = new Student(firstName, lastName, age, homeTown);
+                        students.Add(currentStudent);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid age. Please enter a valid integer for age.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter data in the format: FirstName LastName Age HomeTown");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            string targetCity = Console.ReadLine();
+            List<Student> filteredStudents = students.Where(x => x.HomeTown == targetCity).ToList();
+
+            foreach (Student student in filteredStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+}
