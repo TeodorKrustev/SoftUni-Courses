9fa40d995c03c90826229c022db8c9cd458a45e9
TeodorKrustev <teodorrkrustev@abv.bg>
2024-05-09 10:29:01 +0300

    Add files

---
+++ Programming Advanced for QA/Objects and Classes P2/Students/Students/Program.cs
@@ -0,0 +1,43 @@
+﻿using ObjectsAndClassesExercise;
+
+int countStudents = int.Parse(Console.ReadLine());
+
+List<Student> students = new List<Student>();
+
+for (int count = 1; count <= countStudents; count++)
+{
+    string data = Console.ReadLine();
+    string[] dataParts = data.Split(" ");
+
+    string firstName = dataParts[0];
+    string lastName = dataParts[1];
+    double grade = double.Parse(dataParts[2]);
+
+    Student student = new Student(firstName, lastName, grade);
+    students.Add(student);
+}
+students = students.OrderByDescending(s => s.Grade).ToList();
+
+foreach (Student student in students)
+{
+    Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+}
+namespace ObjectsAndClassesExercise
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        // Constructor
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+
+    }
+}
